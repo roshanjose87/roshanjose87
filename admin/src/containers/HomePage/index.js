b03ffd872d7db0bca75703967aba86cd1040ea52
roshanjose87 <80584887+roshanjose87@users.noreply.github.com>
2021-03-13 19:30:40 +0530

    Add files via upload

---
+++ admin/src/containers/HomePage/index.js
@@ -0,0 +1,21 @@
+/*
+ *
+ * HomePage
+ *
+ */
+/* eslint-disable */
+
+import React, { memo, useMemo } from 'react';
+
+
+const HomePage = ()=>{
+    return(
+        <div>
+           {/* <DisplayJobs></DisplayJobs> */}
+           Hello User
+           <button>Click me</button>
+        </div>
+    )
+}
+
+export default memo(HomePage);
